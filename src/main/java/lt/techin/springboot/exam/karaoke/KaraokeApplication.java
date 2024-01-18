51992a509280a5de72b5e4ddbc9fed02ccb763c5
Rūta Rožėnė <rozene.ruta@gmail.com>
2024-01-18 12:18:30 +0200

    Initial commit

---
+++ src/main/java/lt/techin/springboot/exam/karaoke/KaraokeApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.springboot.exam.karaoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaraokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaraokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/techin/springboot/exam/karaoke/KaraokeApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.springboot.exam.karaoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaraokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
